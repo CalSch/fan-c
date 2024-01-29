3368efa888ba9039f87d942f5f7add617bbd5898
CalSch <cal.schwick@gmail.com>
2024-01-28 22:06:37 -0600

    added fanc.h

---
+++ fanc.h
@@ -0,0 +1,43 @@
+// Keywords
+#define perchance if
+#define otherwise else
+#define whilst while
+#define whereas for
+#define advance continue
+#define bestow return
+#define halt break
+#define perform do
+
+// Types
+// (these are mostly just the real words instead of synonyms)
+#define automatic auto
+#define integer int
+#define decimal float
+#define character char
+#define boolean bool
+#define invariable const
+#define stagnant static
+#define architecture struct
+#define vacant void
+#define categorized enum
+
+// Operations
+#define not !
+#define equals ==
+#define unequal !=
+#define is_greater_than >
+#define is_less_than <
+#define is_greater_than_or_equal_to >=
+#define is_less_than_or_equal_to <=
+#define added_to +
+#define subtract_by -
+#define multiplied_by *
+#define divided_by /
+#define modulus %
+#define increment(i) i++
+#define decrement(i) i--
+#define and &&
+#define or ||
+#define bitwise_and &
+#define bitwise_or |
+#define bitwise_exclusive_or ^
